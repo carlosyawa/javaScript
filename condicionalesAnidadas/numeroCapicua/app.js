7119f373381a043588ac0c57400fef628cc82bdb
Carlos Martinez <cmcarlos874@gmail.com>
2019-09-06 16:54:07 -0500

    numeroCapicua

---
+++ condicionalesAnidadas/numeroCapicua/app.js
@@ -0,0 +1,32 @@
+let btnComparar = document.getElementById('btnComparar');
+
+EventListener();
+
+function EventListener() {
+
+    btnComparar.addEventListener('click', contar);
+
+    inputNumber.addEventListener('blur', calcularCifraBlur);
+}
+
+
+function contar() {
+
+    let numero = Number(document.getElementById('numero').value);
+    let imprimirResultado = document.getElementById('resultado');
+    let unidades = 0;
+    let centenas = 0;
+    let decenas = 0;
+    centenas = Math.floor(numero / 100);
+    decenas = Math.floor((numero % 100) / 10)
+    unidades = Math.floor((numero % 100) % 10)
+
+
+    if (centenas == unidades) {
+        imprimirResultado.innerHTML = ('El numero ' + numero + ' es capicua');
+    } else {
+        imprimirResultado.innerHTML = ('El numero ' + numero + ' no es capicua');
+    }
+
+
+}
